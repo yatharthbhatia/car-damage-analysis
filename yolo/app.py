3f2c90aaf3ac2eb56a2f404e381263d5842da6a4
Yatharth Bhatia <124282341+yatharthbhatia@users.noreply.github.com>
2025-03-06 12:19:08 +0530

    Update app.py for vercel

--- yolo/app.py
+++ yolo/app.py
@@ -12,7 +12,7 @@ app = Flask(__name__)
 
 device = select_device('cpu')
 
-MODEL_PATH = r"models/car_damage5/weights/best.pt"
+MODEL_PATH = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'models', 'car_damage5', 'weights', 'best.pt')
 if not os.path.exists(MODEL_PATH):
     raise FileNotFoundError(f"Model file not found at {MODEL_PATH}.")
 
